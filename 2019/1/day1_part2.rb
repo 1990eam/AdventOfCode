fcc07a5f04085a1ecc93c0e5e084a9afb9cd1dde
Elian Magni <magni.elian@gmail.com>
2020-07-05 13:30:15 -0300

    added comments

--- 2019/1/day1_part2.rb
+++ 2019/1/day1_part2.rb
@@ -17,10 +17,14 @@ What is the sum of the fuel requirements for all of the modules on your spacecra
 
 =end
 
+# hace el calculo general
 def calculate_fuel(fuel)
   return (fuel / 3).round - 2
 end
 
+# si el valor del calculo es negativo, devuelve el mismo fuel que le llega (ejemplo 5 de fuel no necesita ajustarse)
+# si el valor del calculo es positivo, devuelve de manera recursiva el fuel original + el resultado de volver a ajustar sobre ese resultado, y asi sucesivamente.
+# el valor final retornado tendra en cuenta el valor fuel original + su ajuste (se puede apreciar en los outputs, comparando los arrays de fuels y adjustment)
 def adjust(fuel)
   fuel_for_fuel = calculate_fuel(fuel)
   if fuel_for_fuel <= 0
@@ -30,22 +34,27 @@ def adjust(fuel)
   end
 end
 
-
+# file read
 input = IO.readlines("data/input_part1.txt")
+
+# primero calculo los fuels normales
 fuels = []
 input.each do |line|
   fuels << calculate_fuel(line.to_i)
 end
+p "Total fuels are: #{fuels}"
 
-adjust = []
+# despues su ajuste por peso
+adjustment = []
 fuels.each do |fuel|
-  adjust << adjust(fuel)
+  adjustment << adjust(fuel)
 end
+p "Total adjustments are: #{adjustment}"
 
+# por ultimo el total es la suma de todos los adjustments
+# cada adjustment esta compuesto del fuel original + su ajuste
 grand_total = 0
-adjust.each {|fuel|
+adjustment.each {|fuel|
   grand_total += fuel
-
 }
-
-puts grand_total
+p "Grand total Fuel requirement is: #{grand_total}"
